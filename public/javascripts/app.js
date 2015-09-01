9f1fc822330a25af0c153ebc2f9ad90bea0ddb7b
Kim <kimhayden@Kims-MacBook-Air.local>
2015-09-01 10:29:14 -0500

    lots of changes

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
+//When the page loads do an ajax get call to the memes/snark route
+    $.ajax({
+        type: "GET",
+        url: "/memes/snark"
+    }).done(function(data){ // Store snark.json as var data
+        console.log(data);
+        obj = JSON.parse(data); //turn json obj into js array
+        obj.forEach(function(elem, index){  //iterate through obj(snark array)
+            if(elem.id == $('.meme-container').get(elem.id-1))//if the snark id is equal to the index of an image, then we append to the DOM correct position
+            console.log(elem.snark, $('.meme-container').get(elem.id-1));
+        })
+    })
+
+
 
 });
--- routes/memes.js
+++ routes/memes.js
@@ -5,11 +5,38 @@ var path = require('path');
 var memes = require('../models/memes');
 var snark = require('../models/snark');
 
+snark = JSON.stringify(snark);
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 
   res.render('memes', {'memes': memes, 'comments': snark});
 
 });
+
+router.get('/snark', function(req, res, next){
+  res.send(snark);
+});
+
+router.post('/', function(req, res, next) {
+  //This is going to read the snark file and it is going to parse the JSON data and push our request data on to it and write back to the JSON file
+  var file = path.join(__dirname, '../models/snark.json');
+  fs.readFile(file, 'utf8', function (err, data) {
+    if (err) {
+      next(err);
+    }
+    else {
+      var obj = JSON.parse(data);
+      obj.push(req.body);
+      console.log(obj);
+      fs.writeFile(file, JSON.stringify(obj), 'utf-8', function (err) {
+        if (err) return console.log(err);
+        console.log('Wrote Snark');
+
+      })
+    }
+  });
+});
+
 
 module.exports = router;
